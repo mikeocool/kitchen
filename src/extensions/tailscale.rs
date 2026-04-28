d639a2983fc272e96f317f1099156cac20c0232e
Mike O'Toole <mike-git-noreply@keenandbright.dev>
2026-04-28 14:46:39 +0000

    adding tailscale poststart hook

--- src/extensions/tailscale.rs
+++ src/extensions/tailscale.rs
@@ -1,5 +1,42 @@
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
+use std::thread;
+
 use crate::kitchen::Kitchen;
 
-pub fn poststart(kitchen: &Kitchen) -> Result<(), Box<dyn std::error::Error>> {
+pub fn poststart(_kitchen: &Kitchen) -> Result<(), Box<dyn std::error::Error>> {
+    let mut child = Command::new("sudo")
+        .args(["tailscale", "up", "--ssh"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+
+    let stdout = child.stdout.take().unwrap();
+    let stderr = child.stderr.take().unwrap();
+
+    let stdout_thread = thread::spawn(move || {
+        for line in BufReader::new(stdout).lines() {
+            if let Ok(line) = line {
+                println!("{}", line);
+            }
+        }
+    });
+
+    let stderr_thread = thread::spawn(move || {
+        for line in BufReader::new(stderr).lines() {
+            if let Ok(line) = line {
+                eprintln!("{}", line);
+            }
+        }
+    });
+
+    stdout_thread.join().ok();
+    stderr_thread.join().ok();
+
+    let status = child.wait()?;
+    if !status.success() {
+        return Err("tailscale up --ssh failed".into());
+    }
+
     Ok(())
 }
